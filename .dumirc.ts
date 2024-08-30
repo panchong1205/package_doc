db17c8c656a5b9377341a3001a13d3bc49225b24
panchong <953456262@qq.com>
2024-08-30 14:34:35 +0800

    打包后的文件

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+  publicPath: 'https://panchong1205.github.io/panchong.github.io/docs-dist/',
   themeConfig: {
     name: 'pc_packages',
   },
