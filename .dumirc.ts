e9f5269d15a09c5a7076302f0433fddc313fbe3d
panchong <953456262@qq.com>
2024-08-30 15:04:43 +0800

    打包配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'dumi';
 
+
 export default defineConfig({
   outputPath: 'docs-dist',
-  publicPath: 'https://panchong1205.github.io/package_doc/docs-dist/',
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : 'https://panchong1205.github.io/package_doc/docs-dist/',
   themeConfig: {
     name: 'pc1205',
   },
